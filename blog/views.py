548481881e045ae2bf5785d6dd4f3b2fbfb5c9a5
Diyorbek-Java <diorbeckturdiev@gmail.com>
2024-07-16 14:08:22 +0500

    first project is finished

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from .models import Post
 from django.http import Http404
 
@@ -7,4 +7,9 @@ def post_list(request):
     return render(request,'blog/post/list.html',{'posts':posts})
 
 def post_detail(request,id):
-    pass
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
+    return render(request,'blog/post/detail.html',{'post':post})
